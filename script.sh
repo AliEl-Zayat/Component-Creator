229a8e1afca181d4d68ddc1a4baba089e25e2873
Ali El-Zayat <aliel-zayat@192.168.1.4>
2023-11-23 02:13:33 +0200

    Implements menu and index patter for directory

--- script.sh
+++ script.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-defaultOutputPath="C:\Users\Administrator\Desktop\Development\NASNAV\nasnav-react\src\Themes\Yumazing\Components" # Set your default output path here
-
-# Ask for the output path if the default isn't provided in the script
-if [ -z "$defaultOutputPath" ]; then
-    read -p "Enter the default output path: " defaultOutputPath
-fi
-
-if [ -z "$defaultOutputPath" ]; then
-    echo "Please provide a default output path."
-    exit 1
-fi
-
 # Ask if user wants to use TypeScript
 read -p "Using TypeScript? (Yes/No): " useTypescript
 
@@ -23,55 +11,113 @@ else
     indexFile="index.js"
 fi
 
+# Prompt user for the directory path
+read -p "Enter the directory path: " DIRECTORY_PATH
+
+# Check if the directory exists
+if [ ! -d "$DIRECTORY_PATH" ]; then
+    echo "Directory does not exist or is invalid."
+    exit 1
+fi
+
+# Function to create index.js or index.ts based on user selection
+create_index_file() {
+    INDEX_FILE="$DIRECTORY_PATH/$indexFile"
+    echo "" > "$INDEX_FILE"
+
+    # Loop through folders in the directory and generate imports
+    for folder in "$DIRECTORY_PATH"/*/; do
+        folder_name=$(basename "$folder")
+        capitalized_folder_name="$(tr '[:lower:]' '[:upper:]' <<< ${folder_name:0:1})${folder_name:1}"
+        echo "import ${capitalized_folder_name} from \"./${folder_name}\"" >> "$INDEX_FILE"
+        components+=" ${capitalized_folder_name},"
+    done
+
+    # Export components
+    echo -n "export {" >> "$INDEX_FILE"
+    echo -n "$components" | sed 's/,$//' >> "$INDEX_FILE"
+    echo " }" >> "$INDEX_FILE"
+
+    echo "Created $indexFile in $DIRECTORY_PATH"
+}
+
+# Main menu
 while true; do
-    read -p "Enter component name (or 'exit' to quit): " userInput
+    echo "Menu:"
+    echo "1. Loop over folders and create index file"
+    echo "2. Create React components"
+    echo "000. Exit"
 
-    # Extract the component name and check for -ui flag 
-    componentName=$(echo "$userInput" | cut -d' ' -f1)
-    isUiFlag=$(echo "$userInput" | grep -o '\-ui')
+    read -p "Select an option: " option
 
-    # Check if user wants to exit
-    if [ "$componentName" == "exit" ]; then
-        echo "Exiting..."
-        break
-    fi
+    case "$option" in
+        1)
+            # Create the index.js or index.ts file
+            create_index_file
+            ;;
+        2)
+            while true; do
+                read -p "Enter component name (or 'exit' to quit): " userInput
 
-    # Check if componentName is provided
-    if [ -z "$componentName" ]; then
-        echo "Please provide a component name."
-    else
-        # Use default output path
-        outputPath="$defaultOutputPath"
+                # Extract the component name and check for -ui flag 
+                componentName=$(echo "$userInput" | cut -d' ' -f1)
+                isUiFlag=$(echo "$userInput" | grep -o '\-ui')
 
-        # Check for -ui flag in user input
-        if [ ! -z "$isUiFlag" ]; then
-            outputPath="$outputPath/UI"
-            mkdir -p "$outputPath"
-        fi
+                # Check if user wants to exit or go back
+                if [ "$componentName" == "exit" ]; then
+                    echo "Exiting component creation..."
+                    break
+                elif [ "$componentName" == "000" ]; then
+                    echo "Going back to the main menu..."
+                    break 2
+                fi
 
-        # Create folder and files
-        folderPath="$outputPath/$componentName"
-        mkdir -p "$folderPath"
-        touch "$folderPath/$componentName.$fileExtensions"
-        touch "$folderPath/$componentName.module.scss"
-        touch "$folderPath/$indexFile"
+                # Check if componentName is provided
+                if [ -z "$componentName" ]; then
+                    echo "Please provide a component name."
+                else
+                    # Use default output path
+                    outputPath="$DIRECTORY_PATH"
 
-        # Write content to componentName file
-        echo "import React from 'react';" > "$folderPath/$componentName.$fileExtensions"
-        echo "import Styles from './$componentName.module.scss';" >> "$folderPath/$componentName.$fileExtensions"
-        echo "" >> "$folderPath/$componentName.$fileExtensions"
-        echo "const $componentName = () => {" >> "$folderPath/$componentName.$fileExtensions"
-        echo "  return <></>;" >> "$folderPath/$componentName.$fileExtensions"
-        echo "};" >> "$folderPath/$componentName.$fileExtensions"
-        echo "" >> "$folderPath/$componentName.$fileExtensions"
-        echo "export default $componentName;" >> "$folderPath/$componentName.$fileExtensions"
+                    # Check for -ui flag in user input
+                    if [ ! -z "$isUiFlag" ]; then
+                        outputPath="$outputPath/UI"
+                        mkdir -p "$outputPath"
+                    fi
 
-        # Write content to index file
-        echo "export { default } from './$componentName';" > "$folderPath/$indexFile"
+                    # Create folder and files
+                    folderPath="$outputPath/$componentName"
+                    mkdir -p "$folderPath"
+                    touch "$folderPath/$componentName.$fileExtensions"
+                    touch "$folderPath/$componentName.module.scss"
+                    touch "$folderPath/$indexFile"
 
-        echo "Folder structure created for $componentName in $folderPath:"
-        echo "- $folderPath/$componentName.$fileExtensions"
-        echo "- $folderPath/$componentName.module.scss"
-        echo "- $folderPath/$indexFile"
-    fi
+                    # Write content to componentName file
+                    echo "import React from 'react';" > "$folderPath/$componentName.$fileExtensions"
+                    echo "import Styles from './$componentName.module.scss';" >> "$folderPath/$componentName.$fileExtensions"
+                    echo "" >> "$folderPath/$componentName.$fileExtensions"
+                    echo "const $componentName = () => {" >> "$folderPath/$componentName.$fileExtensions"
+                    echo "  return <></>;" >> "$folderPath/$componentName.$fileExtensions"
+                    echo "};" >> "$folderPath/$componentName.$fileExtensions"
+                    echo "" >> "$folderPath/$componentName.$fileExtensions"
+                    echo "export default $componentName;" >> "$folderPath/$componentName.$fileExtensions"
+
+                    # Write content to index file
+                    echo "export { default } from './$componentName';" > "$folderPath/$indexFile"
+
+                    echo "Folder structure created for $componentName in $folderPath:"
+                    echo "- $folderPath/$componentName.$fileExtensions"
+                    echo "- $folderPath/$componentName.module.scss"
+                    echo "- $folderPath/$indexFile"
+                fi
+            done
+            ;;
+        000)
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid option, please try again."
+            ;;
+    esac
 done
